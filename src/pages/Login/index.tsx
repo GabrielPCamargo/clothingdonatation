ba7a7d220f1c03b47b112cb9cb82be65ff50bdbd
Gabriel Pinos Camargo <gabrielpinos03@gmail.com>
2022-06-02 10:13:58 -0300

    feat(login page design done): login page done, but it needs its funtionality yet

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import clothes from '../../assets/img/clothes.png';
+import { Container, Form } from './styled';
 
 export function Login() {
-  return <h1>Login</h1>;
+  return (
+    <Container>
+      <header>
+        <div className="titlebox">
+          <img src={clothes} alt="" />
+          <div>
+            <h1>
+              Clothing
+              <br />
+              Donation
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      <section>
+        <Form>
+          <h2>LOGIN</h2>
+          <label htmlFor="email">
+            E-MAIL
+            <input type="email" name="email" placeholder="example@gmail.com" />
+          </label>
+
+          <label htmlFor="password">
+            SENHA
+            <input type="password" name="password" />
+          </label>
+          <button type="submit">Entrar</button>
+        </Form>
+      </section>
+    </Container>
+  );
 }
---
+++ src/pages/Login/styled.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+
+  header {
+    display: flex;
+    justify-content: center;
+    max-height: 10vh;
+    padding-top: 4rem;
+
+    .titlebox {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex: 1;
+    }
+
+    .titlebox h1 {
+      margin: auto 0;
+      margin-left: 2.5rem;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 36px;
+      line-height: 100%;
+    }
+
+    .titlebox img {
+      height: 100%;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  display: block;
+  margin: 4rem auto;
+  text-align: center;
+  max-width: 25vw;
+
+  h2 {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 125%;
+    margin-bottom: 2rem;
+  }
+
+  label {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 500;
+    line-height: 100%;
+
+    & + label {
+      margin-top: 1.5rem;
+    }
+  }
+
+  input {
+    margin-top: 0.25rem;
+    background: #445275;
+    border: 1px solid #1656fd;
+    border-radius: 6px;
+    padding: 0.5rem;
+    color: #fff;
+    font-size: 1rem;
+  }
+
+  button {
+    margin-top: 4rem;
+    padding: 0.5rem 5rem;
+    background: #0243ec;
+    border-radius: 6px;
+    color: #fff;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 100%;
+  }
+`;
